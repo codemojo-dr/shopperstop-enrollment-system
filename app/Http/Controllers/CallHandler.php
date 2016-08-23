bb22e7a4ff00f00ac73b9eb7088d402f04ffa9d5
Shoaib <shoaib@codemojo.io>
2016-08-24 00:27:33 +0530

    Updated the message

--- app/Http/Controllers/CallHandler.php
+++ app/Http/Controllers/CallHandler.php
@@ -20,7 +20,7 @@ class CallHandler extends Controller
         $number = $this->sanitizeNumber($number);
 
         $sms = app('dial2verify.sms');
-        $message = "Welcome to ShoppersStop. To register your First Citizen Card, click on the link http://shpr.st/reg/ or reply to 9212356765 in the below format"
+        $message = "Welcome to ShoppersStop. To register your First Citizen Card, click on the link http://shpr.st/reg/ or reply to +919212356765 in the below format"
         . "\n\nACTIVATE <Card Number>, <Name>, <Email>, <DOB DD/MM/YY>"
         . "\n\nActivate your card and get 50 points credited to your First Citizen Card";
 
@@ -48,7 +48,10 @@ class CallHandler extends Controller
             $message = "Dear Patron, Please make sure that you have entered all the fields with valid data.\n\nPlease retry again or give us a call at 15221522";
         } else {
             app('codemojo.wallet')->addBalance($card_number, 50, 1, 0, sha1(time() . $number . $card_number), 'Card activation bonus');
-            $message = "Congratulations, your First Citizen Card has been successfully activated. We have credited 50 points to your account & your current balance is 256.45 points
+
+            $balance = app('codemojo.wallet')->getBalance($card_number);
+
+            $message = "Congratulations, your First Citizen Card has been successfully activated. We have credited 50 points to your account & your current balance is {$balance} points
                     \n\nPlease download our app from http://bit.ly/2btk8YU and get additional 50 points on signup";
         }
 
